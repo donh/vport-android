ca15627207be75f3d3a45d06175b97e684f8d544
QuincySx <wang.rongqiang@21vianet.com>
2017-06-16 15:09:00 +0800

    添加 IPFS add

--- app/src/androidTest/java/com/a21vianet/wallet/vport/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/a21vianet/wallet/vport/ExampleInstrumentedTest.java
@@ -1,13 +1,18 @@
 package com.a21vianet.wallet.vport;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.a21vianet.wallet.vport.http.Api;
+import com.a21vianet.wallet.vport.library.commom.http.ipfs.IPFSRequest;
+import com.a21vianet.wallet.vport.library.commom.http.ipfs.bean.AddResult;
+import com.google.gson.Gson;
+import com.littlesparkle.growler.core.http.BaseHttpSubscriber;
+import com.littlesparkle.growler.core.http.ErrorResponse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -17,10 +22,34 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void testIpfsAdd() {
+        String toJson = new Gson().toJson(new User("张三", "0X008975766666666", 1));
+        new IPFSRequest(Api.IPFSApi).ipfsAddJson(new BaseHttpSubscriber<AddResult>() {
+            @Override
+            protected void onError(ErrorResponse error) {
+                super.onError(error);
+            }
 
-        assertEquals("com.a21vianet.wallet.vport", appContext.getPackageName());
+            @Override
+            protected void onSuccess(AddResult addResult) {
+                assertEquals(addResult.getHash(), "QmQD9BZqnabKm7ErxjTPCgV1CxWboQCBkpQFdQZy5h1GgK");
+            }
+        }, toJson);
+    }
+
+    @Test
+    public void testIpfsGet() {
+        final String toJson = new Gson().toJson(new User("张三", "0X008975766666666", 1));
+        new IPFSRequest(Api.IPFSApi).ipfsGetJson(new BaseHttpSubscriber<String>() {
+            @Override
+            protected void onError(ErrorResponse error) {
+                super.onError(error);
+            }
+
+            @Override
+            protected void onSuccess(String s) {
+                assertEquals(s, toJson);
+            }
+        }, "QmQD9BZqnabKm7ErxjTPCgV1CxWboQCBkpQFdQZy5h1GgK");
     }
 }
--- vportlibrary/src/main/java/com/a21vianet/wallet/vport/library/commom/http/ipfs/IPFSRequest.java
+++ vportlibrary/src/main/java/com/a21vianet/wallet/vport/library/commom/http/ipfs/IPFSRequest.java
@@ -2,18 +2,33 @@ package com.a21vianet.wallet.vport.library.commom.http.ipfs;
 
 import android.support.annotation.NonNull;
 
+import com.a21vianet.wallet.vport.library.commom.http.ipfs.bean.AddResult;
 import com.a21vianet.wallet.vport.library.commom.http.ipfs.bean.UserInfoIPFS;
 import com.a21vianet.wallet.vport.library.commom.http.ipfs.bean.UserInfoIPFSGET;
 import com.google.gson.Gson;
 import com.littlesparkle.growler.core.http.Request;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Streaming;
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -46,8 +61,35 @@ public class IPFSRequest extends Request<IPFSRequest.IPFSApi> {
                 .subscribe(subscriber);
     }
 
-    public Subscription get(Subscriber<UserInfoIPFSGET> subscriber, String senderAddress, String proxyAddress) {
+    public Subscription get(Subscriber<UserInfoIPFSGET> subscriber, String senderAddress, String
+            proxyAddress) {
         return mService.get(senderAddress, proxyAddress, "PROFILE")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(subscriber);
+    }
+
+    public Subscription ipfsGetJson(Subscriber<String> subscriber, String hash) {
+        return mService.ipfsGetJson(hash)
+                .map(new Func1<ResponseBody, String>() {
+                    @Override
+                    public String call(ResponseBody responseBody) {
+                        return getResponseBody(responseBody);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(subscriber);
+    }
+
+    public Subscription ipfsAddJson(Subscriber<AddResult> subscriber,
+                                    String json) {
+        RequestBody requestFile = RequestBody.create(MediaType.parse("text/plain"), json);
+        MultipartBody.Part formData = MultipartBody.Part.createFormData("file", "ipfsjson",
+                requestFile);
+        return mService.ipfsAddJson(formData)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .unsubscribeOn(Schedulers.io())
@@ -71,5 +113,41 @@ public class IPFSRequest extends Request<IPFSRequest.IPFSApi> {
                 @Field("proxyAddress") String proxyAddress,
                 @Field("registrationIdentifier") String registrationIdentifier
         );
+
+        @POST("/api/v0/add")
+        @Multipart
+        Observable<AddResult> ipfsAddJson(@Part MultipartBody.Part file);
+
+        @Streaming
+        @GET("/ipfs/{hash}")
+        Observable<ResponseBody> ipfsGetJson(@Path("hash") String hash);
+    }
+
+    private String getResponseBody(ResponseBody body) {
+        try {
+            // 改成自己需要的存储位置
+            InputStream inputStream = null;
+            ByteArrayOutputStream outputStream = null;
+            byte[] fileReader = new byte[4096];
+
+            inputStream = body.byteStream();
+            outputStream = new ByteArrayOutputStream();
+
+            while (true) {
+                int read = inputStream.read(fileReader);
+                if (read == -1) {
+                    break;
+                }
+                outputStream.write(fileReader, 0, read);
+            }
+            outputStream.flush();
+            String json = outputStream.toString();
+            outputStream.close();
+            inputStream.close();
+            return json;
+        } catch (IOException e) {
+
+        }
+        return "";
     }
 }
