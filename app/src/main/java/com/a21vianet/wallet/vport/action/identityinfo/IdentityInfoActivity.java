e289720a92fc6837404f8858a83f398d1c8b3dc4
QuincySx <wang.rongqiang@21vianet.com>
2017-06-20 09:54:51 +0800

    调整状态按钮

--- app/src/main/java/com/a21vianet/wallet/vport/action/identityinfo/IdentityInfoActivity.java
+++ app/src/main/java/com/a21vianet/wallet/vport/action/identityinfo/IdentityInfoActivity.java
@@ -1,6 +1,8 @@
 package com.a21vianet.wallet.vport.action.identityinfo;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.ColorInt;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,9 +16,11 @@ import com.a21vianet.wallet.vport.action.identityinfo.bean.IdentityInfo;
 import com.bumptech.glide.Glide;
 import com.littlesparkle.growler.core.ui.activity.BaseActivity;
 import com.littlesparkle.growler.core.ui.adapter.RecyclerBaseAdapter;
+import com.littlesparkle.growler.core.utility.DensityUtility;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -59,6 +63,7 @@ public class IdentityInfoActivity extends BaseActivity {
         mIdCardAdapter.setDataList(mIdentityInfoList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mIdCardAdapter);
+        mIdCardAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -84,21 +89,54 @@ public class IdentityInfoActivity extends BaseActivity {
     }
 
     static class IdCardAdapter extends RecyclerBaseAdapter<ViewHolder, IdentityInfo> {
+        private final int strokeWidth;
+        private final int roundRadius;
 
         public IdCardAdapter(Context context) {
             super(context);
+            strokeWidth = DensityUtility.dp2px(context, 1);
+            roundRadius = DensityUtility.dp2px(context, 4);
         }
 
         @Override
         protected void onBindViewHolderItem(ViewHolder holder, IdentityInfo item, int position) {
-            holder.mCertificationStatusTextView.setText("待审核");
+
             holder.mIdCardTextView.setText(item.getId());
+
+            String staetStr = "";
+            @ColorInt
+            int typeColor;
+            switch (new Random(3).nextInt()) {
+                case 0:
+                    //待认证
+                    staetStr = "待认证";
+                    typeColor = 0xFF7d7d7d;
+                    break;
+                case 1:
+                    //认证成功
+                    staetStr = "认证成功";
+                    typeColor = 0xFF1b93ef;
+                    break;
+                case 2:
+                default:
+                    //认证失败
+                    staetStr = "认证失败";
+                    typeColor = 0xFFeb212e;
+            }
+
+            holder.mCertificationStatusTextView.setText(staetStr);
+
+            GradientDrawable gradientDrawableType = new GradientDrawable();
+            gradientDrawableType.setCornerRadius(roundRadius);
+            gradientDrawableType.setStroke(strokeWidth, typeColor);
+            holder.mCertificationStatusTextView.setBackground(gradientDrawableType);
+            holder.mCertificationStatusTextView.setTextColor(typeColor);
         }
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View inflate = LayoutInflater.from(mContext).inflate(R.layout.item_identity_info,
-                    null, false);
+                    parent, false);
             return new ViewHolder(inflate);
         }
     }
