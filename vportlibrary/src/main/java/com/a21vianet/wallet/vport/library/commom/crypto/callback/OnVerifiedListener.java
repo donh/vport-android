e57751e4ac167ad22b6cb2ad0c2fe585fdf39010
Don <dontwn@gmail.com>
2017-10-19 04:10:57 -0500

    Revert "init code"

--- vportlibrary/src/main/java/com/a21vianet/wallet/vport/library/commom/crypto/callback/OnVerifiedListener.java
+++
@@ -1,9 +0,0 @@
-package com.a21vianet.wallet.vport.library.commom.crypto.callback;
-
-/**
- * Created by ben on 2017/6/20.
- */
-
-public interface OnVerifiedListener {
-    void onVerified(boolean isValid);
-}
